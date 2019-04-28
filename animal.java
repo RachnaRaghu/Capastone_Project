98069c7fb6c38db4619d3da1e9fb9b4b948b57e9
RachnaRaghu <50066933+RachnaRaghu@users.noreply.github.com>
2019-04-28 11:11:05 +0530

    Add files via upload

---
+++ animal.java
@@ -0,0 +1,43 @@
+interface animal
+{
+	void noise();
+}
+
+class cat implements animal 
+{
+	public void noise()
+	{
+      System.out.println("meow -meow");
+	}
+}
+class dog implements animal 
+{
+	public void noise()
+	{
+      System.out.println("bhow-bhow");
+	}
+}
+class snake implements animal 
+{
+	public void noise()
+	{
+      System.out.println("hiss -hiss");
+	}
+}
+class animalsimulator
+{
+	void makenoise(animal arg)
+	{
+		arg.noise();
+	}
+}
+class main12
+{
+	public static void main(String[] args) 
+	{
+		animalsimulator ani = new animalsimulator();
+		ani.makenoise(new cat());
+        ani.makenoise(new dog());
+		ani.makenoise(new snake());
+	}
+}
